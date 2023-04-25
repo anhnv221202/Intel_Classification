36246a621dfe29642932a2f5567970436ed2f07f
VAN221202 <anh.nv221202@gmail.com>
2023-04-25 21:53:07 +0700

    unavailable

--- models/myinceptionnet.py
+++ models/myinceptionnet.py
@@ -4,6 +4,8 @@ from torch.utils.data import Dataset, DataLoader, Subset
 from torchvision.models import Inception3
 from torchsummary import summary
 
+### CURRENTLY UNAVAILABLE, CHECK IN LATER FOR FUTURE FIXES
+
 class BIInceptionV3(Inception3):
     def __init__(self, num_classes: int = 6, aux_logits: bool = True, transform_input: bool = False, inception_blocks= None, init_weights= None, dropout: float = 0.5) -> None:
         super(BIInceptionV3, self).__init__(num_classes, aux_logits, transform_input, inception_blocks, init_weights, dropout)
